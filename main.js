d7c1d4e5ed0b672d17aa8ad440c0147620dced62
Gabriel Caetano <caetano9650@gmail.com>
2023-07-11 16:05:44 -0300

    Commit inicial

---
+++ main.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $('header button').click(function(){
+        $('form').slideDown();
+    })
+    
+    $('#btn-cancelar').click(function(){
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+        const novaIgmAdd = $('#endereco-nova-img').val();
+        const novoItem = $('<li style="display: none;"></li>');
+        $(`<img src="${novaIgmAdd}">`).appendTo(novoItem);
+        $(`
+            <div class="overlay-img-link">
+                <a href="${novaIgmAdd}" target="_blank" title="Ver imagem em tamanho real">
+                    Ver imagem em tamanho real
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn(1000);
+        $('#endereco-nova-img').val(" ")
+    })
+
+})
